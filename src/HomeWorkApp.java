a005566d87dacda3f70b5249607f66a7b36bc51f
mihaelzakharov <mihaelzakharovqa@gmail.com>
2022-05-17 00:15:57 +0400

    Lesson1

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,34 @@
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+
+        printThreeWords();
+        checkSumSign();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 12;
+        int b = 15;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+
+    }
+
+    public static void printColor() {
+
+    }
+
+    public static void compareNumbers() {
+
+    }
+}
